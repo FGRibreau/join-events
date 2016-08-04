916d71991dece7c3112be2fae998b9c297398474
FG Ribreau <github@fgribreau.com>
2016-08-04 18:09:55 +0200

    fix(join)

---
+++ join.js
@@ -0,0 +1,23 @@
+// (em, ...rest, f)
+function join(source, ...rest) {
+  const f = rest.pop();
+  const eventNames = rest;
+
+  // sentinel
+  const EVENT_NOT_SET = {};
+
+  // contains last values for each events, if equals to "EVENT_NOT_SET" it means the events was never received
+  const lastValues = eventNames.map(() => EVENT_NOT_SET);
+
+  eventNames.forEach((event, i) => {
+    source.on(event, (...args) => {
+      lastValues[i] = args.length > 1 ? args : args[0];
+
+      if(lastValues.every(val => val !== EVENT_NOT_SET)){
+        f.apply(null, lastValues);
+      }
+    });
+  });
+}
+
+module.exports = join;
--- join.test.js
+++ join.test.js
@@ -4,27 +4,7 @@ const EventEmitter2 = require('eventemitter2').EventEmitter2;
 const t = require('chai').assert;
 const sinon = require('sinon');
 
-// (em, ...rest, f)
-function join(source, ...rest) {
-  const f = rest.pop();
-  const eventNames = rest;
-
-  // sentinel
-  const EVENT_NOT_SET = {};
-
-  // contains last values for each events, if equals to "EVENT_NOT_SET" it means the events was never received
-  const lastValues = eventNames.map(() => EVENT_NOT_SET);
-
-  eventNames.forEach((event, i) => {
-    source.on(event, (...args) => {
-      lastValues[i] = args.length > 1 ? args : args[0];
-
-      if(lastValues.every(val => val !== EVENT_NOT_SET)){
-        f.apply(null, lastValues);
-      }
-    });
-  });
-}
+const join = require('.');
 
 describe('join', () => {
   const E1 = 'E1';
